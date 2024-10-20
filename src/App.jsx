82bf5247e1d4a36753850c7a77231e304433b72f
Ritik Umang <ritik.umang.bvp@gmail.com>
2024-10-20 22:20:19 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    // <div>App</div>
+    <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+  )
+}
+
+export default App
