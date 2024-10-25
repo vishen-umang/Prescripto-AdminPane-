f8b3e3810fe2101def7396f66de27632aa1add64
Ritik Umang <ritik.umang.bvp@gmail.com>
2024-10-25 23:42:10 +0530

    fifth

--- src/context/AdminContext.jsx
+++ src/context/AdminContext.jsx
@@ -1,22 +1,48 @@
 import { createContext, useState } from "react";
+import axios from "axios";
+import { toast } from "react-toastify";
 
-export const AdminContext = createContext()
+export const AdminContext = createContext();
 
 const AdminContextProvider = (props) => {
+  const [aToken, setAToken] = useState(
+    localStorage.getItem("aToken") ? localStorage.getItem("aToken") : ""
+  );
+  const [doctors, setDoctors] = useState([]);
 
-    const [aToken,setAToken] = useState(localStorage.getItem('aToken') ? localStorage.getItem('aToken') :'')
-    const backendUrl = import.meta.env.VITE_BACKEND_URL
+  const backendUrl = import.meta.env.VITE_BACKEND_URL;
 
-    const value = {
-        aToken,setAToken,
-        backendUrl,
+  const getAllDoctors = async () => {
+    try {
+      const { data } = await axios.post(
+        backendUrl + "/api/admin/all-doctors",
+        {},
+        { headers: { aToken } }
+      );
+      if (data.success) {
+        setDoctors(data.doctors);
+        console.log(data.doctors);
+      } else {
+        toast.error(data.message);
+      }
+    } catch (error) {
+      toast.error(error.message);
     }
+  };
 
-    return (
-        <AdminContext.Provider value={value}>
-            {props.children}
-        </AdminContext.Provider>
-    )
-}
+  const value = {
+    aToken,
+    setAToken,
+    backendUrl,
+    doctors,
+    getAllDoctors,
+  };
 
-export default AdminContextProvider
+  return (
+    <AdminContext.Provider value={value}>
+      {props.children}
+    </AdminContext.Provider>
+  );
+};
+
+export default AdminContextProvider;
--- src/pages/Admin/DoctorList.jsx
+++ src/pages/Admin/DoctorList.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import React, { useContext, useEffect } from 'react'
+import { AdminContext } from '../../context/AdminContext'
 
 const DoctorList = () => {
+  const {doctors, aToken, getAllDoctors } = useContext(AdminContext)
+
+   useEffect(()=>{
+    if(aToken) {
+      getAllDoctors
+    }
+   },[aToken])
+
+
   return (
     <div>
-        
+        doctors[0];
     </div>
   )
 }
